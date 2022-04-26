d18949b3f5a72e96df2fe071e2ab13ff98b7362c
Flavioerick <104401922+Flavioerick@users.noreply.github.com>
2022-04-25 20:31:48 -0400

    Add files via upload

---
+++ Atividade/src/pacote/Quato.java
@@ -0,0 +1,14 @@
+package pacote;
+
+
+public class Quarto {
+	int numero;
+	double diaria;
+	boolean situacao;
+	
+	public void setQuarto(int numero, double diaria, boolean situacao) {
+		
+	}
+	
+	
+}
